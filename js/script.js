bc0892f514a5165ed8389d8260c3fc20a9862447
CristianoPaiva <159495402+CristianoPaiva@users.noreply.github.com>
2024-03-05 20:26:11 -0300

    Versão final
    Finalização do projeto, com responsividade e javascript introduzidos.

---
+++ js/script.js
@@ -0,0 +1,46 @@
+//alert('JS Carregado');
+
+//Seleção de DOM
+//console.log(document);
+var btnMenu = document.querySelector('.btn-menu');
+var menu = document.querySelector('.menu');
+
+var btnPopup = document.querySelector('.close');
+var bgPopup = document.querySelector('.bg-popup');
+
+//console.log(btnMenu);
+//console.log(menu);
+
+//Eventos (São ações geralmente causadas pelo usuário)
+
+/*
+function boasVindas(){
+    alert('Olá!');
+}
+
+//Ao clicar no btnMenu, chama a função "boasVindas"
+btnMenu.addEventListener('click',boasVindas);
+*/
+
+
+//Ao clicar no btnMenu, chama uma função anônima 
+btnMenu.addEventListener('click', function(){
+    
+    //add ou remove a classe menu-open
+    menu.classList.toggle('menu-open');
+    btnMenu.classList.toggle('x');
+
+})
+
+menu.addEventListener('click', function(){
+    menu.classList.remove('menu-open');
+    btnMenu.classList.remove('x');    
+})
+
+//Fechar toda a estrutura do popup ao clicar no botao x
+
+btnPopup.addEventListener('click', function(){
+   // bgPopup.classList.add('bg-popup-none');
+   bgPopup.style.display = 'none';
+      
+})
